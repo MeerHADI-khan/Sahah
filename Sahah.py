cb1e20d554c0a31c9183464c7c3d497f6de6141f
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-22 20:13:58 +0500

    Sahah.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pip install requests
    pip install mechanize
    pip install bs4
    rm -rf Sahah
    git clone --depth=1 https://github.com/MeerHADI-khan/Sahah.git
    cd Sahah
    python Sahah.py

---
+++ Sahah.py
@@ -0,0 +1,32 @@
+# SAMI
+
+<b></b> </br> <br>[![Github-](https://img.shields.io/badge/Github-.Sahah-dimgray?style=flat-square&logo=github)](https://github.com/MeerHADI-khan/Sahah.git)<br> [![Facebook](https://img.shields.io/badge/Facebook-Sahah-blue?style=flat-square&logo=Facebook-Sahah-blue)](https://www.facebook.com/SAMI.)Xd<br> [![Whatsapp](https://img.shields.io/badge/whatsapp-HADI-deepgreen-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
+
+
+
+<h1 align="center"> [.Sahah]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+ _CLONE FULL OK IDZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf Sahah`
+- `git clone --depth=1 https://github.com/MeerHADI-khan/Sahah.git`
+- `cd Sahah`
+- `python Sahah.py`
+
+
+
+ ___This Tools is Paid___</br>
+ [![Whatsapp](https://img.shields.io/badge/whatsapp-HADI-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
